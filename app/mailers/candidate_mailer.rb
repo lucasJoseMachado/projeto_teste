88bf3efa55df09b4eb25ea79738a5c22a0e4be65
Lucas Machado <lucas.machado@peopleone.com>
2015-10-02 12:00:36 -0300

    conteúdo dos emails

--- app/mailers/candidate_mailer.rb
+++ app/mailers/candidate_mailer.rb
@@ -1,7 +1,12 @@
 class CandidateMailer < ApplicationMailer
   def developer_type_email candidate_type, candidate_email
     subject = I18n.t("candidate_mailer.developer_type_email.subject")
-    content = I18n.t("candidate_mailer.developer_type_email.content", type: candidate_type)
+    if candidate_type == 'generic'
+      content = I18n.t("candidate_mailer.developer_type_email.content.without_area")
+    else
+      type = I18n.t("candidate.developer_type.#{candidate_type}")
+      content = I18n.t("candidate_mailer.developer_type_email.content.with_area", type: type)
+    end
     mail to: candidate_email, subject: subject, body: content
   end
 end
