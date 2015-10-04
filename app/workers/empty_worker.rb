fd926a7fab8c33a4e2cbae619bd0471b82e651b9
Lucas Machado <lucas.machado@peopleone.com>
2015-10-04 20:55:00 -0300

    testando cartridge para o openshift

--- app/workers/empty_worker.rb
+++
@@ -1,7 +0,0 @@
-# criado apenas para fazer o cartridge do sidekiq funcionar no openshift, pois é necessário ao menos um worker na pasta de workers
-class EmptyWorker
-  include Sidekiq::Worker
-
-  def perform
-  end
-end
