813fe0596b3692a6ad4b303cc919475a507d980e
Lucas Machado <lucas.machado@peopleone.com>
2015-10-04 20:44:04 -0300

    apenas corrindo nome da variável para o redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
-if ENV['REDISTOGO_URL'].present?
+if ENV['REDIS_URL'].present?
   Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDISTOGO_URL'] }
+    config.redis = { url: ENV['REDIS_URL'] }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDISTOGO_URL'] }
+    config.redis = { url: ENV['REDIS_URL'] }
   end
 end
